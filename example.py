40a29906254f21878ffce1b23428070e7e881d88
yuzuki <barumu.is.god@gmail.com>
2025-12-31 00:55:04 +0900

    最初のテスト

---
+++ example.py
@@ -0,0 +1,19 @@
+print("hello world")
+def greet():
+    print("こんにちわ")
+
+greet()
+
+def print_name(name):
+    print(f"私の名前は{name}です")
+
+print_name("yuzuki")
+
+def get_greet():
+    return "おはようございます"
+
+print(get_greet())
+
+def add(a, b):
+    return a + b
+print(add(5, 6))
